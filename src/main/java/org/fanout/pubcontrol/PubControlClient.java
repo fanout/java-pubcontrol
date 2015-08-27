c23825d80ccbc2a62dc1ea57a69cec807b7bb6d3
Konstantin Bokarius <bokarius@comcast.net>
2015-08-26 18:12:01 -0700

    added methods to pubcontrolclient

--- src/main/java/org/fanout/pubcontrol/PubControlClient.java
+++ src/main/java/org/fanout/pubcontrol/PubControlClient.java
@@ -14,9 +14,9 @@ import java.io.UnsupportedEncodingException;
 import java.security.Key;
 import javax.xml.bind.DatatypeConverter;
 import javax.crypto.spec.SecretKeySpec;
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.SignatureAlgorithm;
-//import io.jsonwebtoken.impl.crypto.MacProvider;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider;
 
 // The PubControlClient class allows consumers to publish either synchronously
 // or asynchronously to an endpoint of their choice. The consumer wraps a Format
@@ -24,12 +24,12 @@ import javax.crypto.spec.SecretKeySpec;
 // methods. The async publish method has an optional callback parameter that
 // is called after the publishing is complete to notify the consumer of the
 // result.
-public class PubControlClient {
+public class PubControlClient implements Runnable {
     private String uri;
     private final Lock lock = new ReentrantLock();
     private final Lock pubWorkerLock = new ReentrantLock();
-    private final Condition pubWorkerCond = lock.newCondition();
-    private PubWorker pubWorker;
+    private final Condition pubWorkerCond = this.pubWorkerLock.newCondition();
+    private Thread pubWorker;
     private Deque<Object[]> reqQueue = new LinkedList<Object[]>();
     private String authBasicUser;
     private String authBasicPass;;
@@ -74,7 +74,7 @@ public class PubControlClient {
         uri = this.uri;
         auth = this.genAuthHeader();
         this.lock.unlock();
-        //this.pubcall(uri, auth, exports);
+        this.pubCall(uri, auth, exports);
     }
 
     // The asynchronous publish method for publishing the specified item to the
@@ -121,8 +121,8 @@ public class PubControlClient {
     // the consumer makes an asynchronous publish call.
     private void ensureThread() {
         if (this.pubWorker == null) {
-            this.pubWorker = new PubWorker();
-            this.pubWorker.run();
+            this.pubWorker = new Thread(this);
+            this.pubWorker.start();
         }
     }
 
@@ -130,7 +130,7 @@ public class PubControlClient {
     // publishing queue. This method will also activate the pubworker worker
     // thread to make sure that it process any and all requests added to
     // the queue.
-    public void queueReq(Object[] req) {
+    private void queueReq(Object[] req) {
         this.pubWorkerLock.lock();
         this.reqQueue.addLast(req);
         this.pubWorkerCond.signal();
@@ -150,33 +150,58 @@ public class PubControlClient {
             } catch (UnsupportedEncodingException exception) { }
         }
         else if (this.authJwtClaim != null) {
-
+            Key decodedKey = new SecretKeySpec(this.authJwtKey,
+                    SignatureAlgorithm.HS256.getJcaName());
+            Map<String, Object> claims = new HashMap<String, Object>();
+            claims.putAll(this.authJwtClaim);
+            if (this.authJwtClaim.get("exp") == null)
+                claims.put("exp", new Date((new Date()).getTime() + (3600 * 1000)));
+            String token = Jwts.builder().setClaims(claims).
+                    signWith(SignatureAlgorithm.HS256, decodedKey).compact();
+            return "Bearer" + token;
         }
 
         return null;
     }
 
-    /*
-        def gen_auth_header
-            if !@auth_basic_user.nil?
-                return 'Basic ' + Base64.encode64(
-                        "//{@auth_basic_user}://{@auth_basic_pass}")
-            elsif !@auth_jwt_claim.nil?
-                if !@auth_jwt_claim.key?('exp')
-                    claim = @auth_jwt_claim.clone
-                    claim['exp'] = PubControlClient.timestamp_utcnow + 3600
-                else
-                    claim = @auth_jwt_claim
-                end
-                return 'Bearer ' + JWT.encode(claim, @auth_jwt_key)
-            else
-                return nil
-            end
-        end
-    end
+    // An internal method for publishing a batch of requests. The requests are
+    // parsed for the URI, authorization header, and each request is published
+    // to the endpoint. After all publishing is complete, each callback
+    // corresponding to each request is called (if a callback was originally
+    // provided for that request) and passed a result indicating whether that
+    // request was successfully published.
+    @SuppressWarnings({"unchecked"})
+    private void pubBatch(List<Object[]> reqs) {
+        if (reqs.size() == 0)
+            return;
+        String authHeader = (String)reqs.get(0)[1];
+        List<Map<String, Object>> items = new ArrayList<Map<String, Object>>();
+        List<PublishCallback> callbacks = new ArrayList<PublishCallback>();
+        for (Object[] req : reqs) {
+            items.addAll((List<Map<String, Object>>)req[2]);
+            callbacks.add((PublishCallback)req[3]);
+        }
+        boolean result = true;
+        String message = null;
+        try {
+            this.pubCall(uri, authHeader, items);
+        } catch (Exception exception) {
+            message = exception.getMessage();
+        }
+        for (PublishCallback callback : callbacks)
+            if (callback != null)
+                callback.completed(result, message);
+    }
+
     // An internal method for preparing the HTTP POST request for publishing
     // data to the endpoint. This method accepts the URI endpoint, authorization
     // header, and a list of items to publish.
+    public void pubCall(String uri, String authHeader,
+            List<Map<String, Object>> items) {
+
+    }
+
+    /*
     def pubcall(uri, auth_header, items)
         uri = URI(uri + '/publish/')
         content = Hash.new
@@ -205,39 +230,6 @@ public class PubControlClient {
         return response
     end
 
-    // An internal method for publishing a batch of requests. The requests are
-    // parsed for the URI, authorization header, and each request is published
-    // to the endpoint. After all publishing is complete, each callback
-    // corresponding to each request is called (if a callback was originally
-    // provided for that request) and passed a result indicating whether that
-    // request was successfully published.
-    def pubbatch(reqs)
-        raise 'reqs length == 0' unless reqs.length > 0
-        uri = reqs[0][0]
-        auth_header = reqs[0][1]
-        items = Array.new
-        callbacks = Array.new
-        reqs.each do |req|
-            if req[2].is_a? Array
-                items = items + req[2]
-            else
-                items.push(req[2])
-            end
-            callbacks.push(req[3])
-        end
-        begin
-            pubcall(uri, auth_header, items)
-            result = [true, '']
-        rescue => e
-            result = [false, e.message]
-        end
-        callbacks.each do |callback|
-            if !callback.nil?
-                callback.call(result[0], result[1])
-            end
-        end
-    end
-
 */
 
     // An internal class that is meant to run as a separate thread and process
@@ -245,36 +237,35 @@ public class PubControlClient {
     // publishes requests in batches containing a maximum of 10 requests. The
     // method completes and the thread is terminated only when a 'stop' command
     // is provided in the request queue.
-    private class PubWorker extends Thread {
-        public void run() {
-            /*
-            def pubworker
-                quit = false
-                while !quit do
-                    @thread_mutex.lock
-                    if @req_queue.length == 0
-                        @thread_cond.wait(@thread_mutex)
-                        if @req_queue.length == 0
-                            @thread_mutex.unlock
-                            next
-                        end
-                    end
-                    reqs = Array.new
-                    while @req_queue.length > 0 and reqs.length < 10 do
-                        m = @req_queue.pop_front
-                        if m[0] == 'stop'
-                            quit = true
-                            break
-                        end
-                        reqs.push([m[1], m[2], m[3], m[4]])
-                    end
-                    @thread_mutex.unlock
-                    if reqs.length > 0
-                        pubbatch(reqs)
-                    end
-                end
-            end
-            */
+    public void run() {
+        boolean quit = false;
+        while (!quit) {
+            this.pubWorkerLock.lock();
+            if (this.reqQueue.size() == 0) {
+                try {
+                    this.pubWorkerCond.await();
+                    if (this.reqQueue.size() == 0) {
+                        this.pubWorkerLock.unlock();
+                        continue;
+                    }
+                } catch (InterruptedException exception) {
+                    this.pubWorkerLock.unlock();
+                    continue;
+                }
+            }
+            List<Object[]> reqs = new ArrayList<Object[]>();
+            while (this.reqQueue.size() > 0 && reqs.size() < 10) {
+                Object[] m = this.reqQueue.removeFirst();
+                if (m[0] == "stop") {
+                    quit = true;
+                    break;
+                }
+                Object[] req = {m[1], m[2], m[3], m[4]};
+                reqs.add(req);
+            }
+            this.pubWorkerLock.unlock();
+            if (reqs.size() > 0)
+                this.pubBatch(reqs);
         }
     }
 }
